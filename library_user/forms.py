f6737ceeb8af5f7a1e1ce81b9ddcccbcf9ddfba2
H-M-Nizum <hmnizum1714032@gmail.com>
2024-02-04 16:40:58 +0600

    Signup and signin page design

--- library_user/forms.py
+++ library_user/forms.py
@@ -36,10 +36,7 @@ class RegisterForm(UserCreationForm):
             self.fields[field].widget.attrs.update({
                 
                 'class' : (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 rounded '
-                    'py-3 px-4 leading-tight focus:outline-none '
-                    'focus:bg-white focus:border-gray-500'
+                    'p-2 rounded'
                 ) 
             })
 
